972af8a578522d85329567b2b5e3b2a1a44174d2
franRei <38581197+franRei@users.noreply.github.com>
2020-05-23 12:01:37 +0200

    Update Reader.java

--- src/Reader.java
+++ src/Reader.java
@@ -1,9 +1,11 @@
 public class Reader {
 
     public static void main(String[] args) {
-        //Database_Writer writer = new Database_Writer();
+        //change language family
+        String language_family = "Reconstruction:Proto-Germanic";
+        //XML file of wiki
         String file = "enwiktionary_01_01.xml";
         Read_XML reader = new Read_XML();
-        reader.parseFile(file);
+        reader.parseFile(file, language_family);
     }
 }
